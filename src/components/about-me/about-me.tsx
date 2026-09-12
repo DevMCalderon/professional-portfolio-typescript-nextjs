b9eff03c85a1050e060f0926ef352ab506d878eb
ThePushMaker <elmcwl@gmail.com>
2026-09-11 20:12:09 -0700

    style: add CSS classes to the about-me image

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -6,44 +6,60 @@ export default function AboutMe() {
   return (
     <section id="profile">
       <Container className="flex items-center justify-between">
-        {/* left side */}
+        {/* Left side */}
         <div className="w-1/2">
-          <p>¿Quién está detrás de esas soluciones?</p>
-          <h1>Martín Calderón: Ingeniero en Software</h1>
+          {/* Presentation */}
+          <div>
+            <p>¿Quién está detrás de esas soluciones?</p>
+            <h1>Martín Calderón: Ingeniero en Software</h1>
 
-          <p>
-            Desarrollador Full-Stack especializado en Next.js, Laravel y
-            arquitectura de software con +3 años de experiencia freelance
-            construyendo software con tecnologías de última generación.
-          </p>
+            <p>
+              Desarrollador Full-Stack especializado en Next.js, Laravel y
+              arquitectura de software con +3 años de experiencia freelance
+              construyendo software con tecnologías de última generación.
+            </p>
+          </div>
+          {/* Experience */}
+          <div>
+            <h2>Experiencia</h2>
+            <p>
+              Experiencia creando plataformas ecommerce y sistemas de gestión
+              para universidades, veterinarias, cafeterías y otros negocios.
+            </p>
+          </div>
 
-          <ul>
-            <li>
+          {/* Way of working */}
+          <div>
+            <h2>Forma de trabajo</h2>
+            <ul>
+              <li>
+                Trabajo en todas las fases del desarrollo: levantamiento de
+                requisitos, diseño, implementación, pruebas y despliegue.
+              </li>
+              <li>Comunicación proactiva con clientes y equipos remotos.</li>
+              <li>Enfocado en la calidad.</li>
+            </ul>
+          </div>
+
+          {/* Philosophy */}
+          <div>
+            <h2>Filosofía</h2>
+            <p>
               Me gustan los retos, me gusta la incomodidad. Mi ley de vida es:
               "Si te propones algo, trabaja con todo lo que tienes hasta
               lograrlo."
-            </li>
-            <li>
-              Experiencia creando plataformas ecommerce, sistemas de gestión
-              para universidades, veterinarias, cafeterías y otros negocios.
-            </li>
-            <li>
-              Trabajo en todas las fases del desarrollo: levantamiento de
-              requisitos, diseño, implementación, pruebas y despliegue.
-            </li>
-            <li>Comunicación proactiva con clientes y equipos remotos.</li>
-            <li>Enfocado en la calidad.</li>
-            <li>
+            </p>
+            <p>
               Apasionado de la ingeniería. Aspiro a dominar todo lo necesario
               para construir software de alto nivel.
-            </li>
-            <li>
+            </p>
+            <p>
               En la actualidad, dedico la mayor parte de mi tiempo al desarrollo
               de habilidades que potencien mi desempeño profesional.
-            </li>
-          </ul>
+            </p>
+          </div>
 
-          {/* main links */}
+          {/* Main links */}
           <div>
             <Link href={process.env.NEXT_PUBLIC_CV_URL ?? "#"}>
               Descargar mi currículum vitae
@@ -55,20 +71,22 @@ export default function AboutMe() {
             </Link>
           </div>
 
-          {/* other links */}
+          {/* More links */}
           <nav aria-label="Otros enlaces de interés">
             <Link href="...">LinkedIn</Link>
             <Link href="...">GitHub</Link>
           </nav>
         </div>
 
-        {/* right side */}
+        {/* Right side */}
         <div className="w-1/2 flex justify-end">
+          {/* TODO: reemplazar por ImageReflection */}
           <Image
             src="/images/profile.png"
             alt="Martín Calderón"
-            width={280}
-            height={280}
+            width={290}
+            height={290}
+            className="rounded-full border-4 border-background opacity-90 shadow-2xl ml-auto bg-primary w-[70%] md:w-1/2 lg:w-[20rem] mt-16 lg:mt-0 "
           />
         </div>
       </Container>
---
+++ src/components/image-reflection.tsx
@@ -0,0 +1,3 @@
+export default function ImageReflection() {
+  return <>ImageReflection</>;
+}
